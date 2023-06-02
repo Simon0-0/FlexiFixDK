954353a1acb8396ec2755bd0883b2ff6b4e3f0cf
Luigidls <10180259@ucn.dk>
2023-06-02 19:04:12 +0200

    footer-beginning

--- flexifix/footer.php
+++ flexifix/footer.php
@@ -1,7 +1,7 @@
 <footer>
 
     <nav class="footer-menu">
-    <?php the_custom_logo();?>
+        <?php the_custom_logo();?>
         <?php wp_nav_menu(array(
             'theme_location' => 'footerMenu',
             'depth' => 1
@@ -19,21 +19,22 @@
         <div>
             <h4>Contact</h4>
             <h5>
-+45 91 86 95 25 
-flexi-fix@mail.com</h5>
+                +45 91 86 95 25
+                flexi-fix@mail.com</h5>
         </div>
         <div>
             <h5>Opening hours
 
-Monday - Friday: 10 - 18
-Saturday: 10 - 14
-Other times: Appointments only
-Holidays: Usually open, but text us</h5>
+                Monday - Friday: 10 - 18
+                Saturday: 10 - 14
+                Other times: Appointments only
+                Holidays: Usually open, but text us</h5>
         </div>
         <div>
             <img src="http://flexifix.local/wp-content/uploads/2023/05/vintage-bicycle.svg" alt="">
-            <a href=""><h4>facebook</h4></a>
-
+            <a href="">
+                <h4>facebook</h4>
+            </a>
         </div>
     </nav>
 </footer>
